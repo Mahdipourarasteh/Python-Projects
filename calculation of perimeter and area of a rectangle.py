476f26401273c87da7f03fd860fd8177fdfc8d59
Mahdipourarasteh <mahdi.mahdipourarasteh@gmail.com>
2022-05-06 23:44:38 +0200

    1st version

---
+++ calculation of perimeter and area of a rectangle.py
@@ -0,0 +1,9 @@
+print("Hallo liebe/r Benutzer/in.")
+print("Bitte geben Sie die erste Kantenlänge des Rechtecks ein:")
+a=float(input())
+print("Bitte geben Sie die zweite Kantenlänge des Rechtecks ein:")
+b=float(input())
+u=2*(a+b)
+f=a*b
+print("Umfang=",u)
+print("Flächeninhalt=",f)
